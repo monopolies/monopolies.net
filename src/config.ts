1644c43699dc97ee7a2389d6ac1e2a1a8dc558a0
Eliot Bendinelli <eliot.bendinelli@protonmail.com>
2025-09-02 15:24:56 +0200

    Building first components

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export const SITE = {
+  website: "https://monopolies.net/", // replace this with your deployed domain
+  author: "Data Rights",
+  profile: "https://monopolies.net/",
+  desc: "Know which brands belong to big companies, and who owns your digital life.",
+  title: "Digital Monopolies",
+  //ogImage: "favicon.svg",
+  lightAndDarkMode: false,
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  timezone: "Europe/Berlin", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+} as const;
