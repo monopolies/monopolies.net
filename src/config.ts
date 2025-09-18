fee149ecdc2a4d6fc59962131730906c27255159
Eliot Bendinelli <eliot.bendinelli@protonmail.com>
2025-09-18 15:55:56 +0200

    Adding footer

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ export const SITE = {
   website: "https://monopolies.net/", // replace this with your deployed domain
   author: "Data Rights",
   profile: "https://monopolies.net/",
+  repository: "https://github.com/monopolies/",
   description: "Know which brands belong to big companies, and who owns your digital life.",
   title: "Digital Monopolies",
   //ogImage: "favicon.svg",
